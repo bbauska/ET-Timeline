fb34906fdeddbd4c34541b72b3967c0b12d99b92
Brian Bauska <brianbauska@gmail.com>
2026-02-23 23:00:04 -0800

    Add tooltip functionality to posts in script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* js/script.js of et-timeline.bauska.org */
+$(document).ready(function() {
+  var my_posts = $("[rel=tooltip]");
+
+  var size = $(window).width();
+  for(i=0;i<my_posts.length;i++) {
+    the_post = $(my_posts[i]);
+
+    if(the_post.hasClass('invert') && size >=767 ) {
+      the_post.tooltip({ placement: 'left'});
+      the_post.css("cursor","pointer");
+    } else {
+      the_post.tooltip({ placement: 'rigth'});
+      the_post.css("cursor","pointer");
+    }
+  }
+});
